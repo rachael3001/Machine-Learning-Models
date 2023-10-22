050cf1e40f077b7100d04f5718e462ede165bc49
rachael3001 <123117247+rachael3001@users.noreply.github.com>
2023-10-22 21:41:10 +0100

    Created regression model data

---
+++ Monthly Rate/RegressionModel.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge, RidgeCV, Lasso
+from sklearn.preprocessing import StandardScaler 
+
+df = pd.read_csv("insurance.csv")
+
+#Checks for missing values
+print(df.isna().sum().sort_values())
+
+df = pd.get_dummies(df, drop_first=True, dtype=int)
+
+#Finds relationship between categories
+plt.figure(figsize = (10, 10))
+sns.heatmap(df.corr(), annot = True)
+#plt.show()
+#Finds smokers have highest relationship, then age then BMI will focus on those
+
+y = df["charges"].values
+X = df.drop("charges", axis=1).values
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=17)
+
+#Scale the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+#Ran LinearRegression()
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+y_pred  = lr.predict(X_test)
+test_score_lr = lr.score(X_test, y_test)
+print("The test score for lr model is {}".format(test_score_lr))
+
+
+ridgeReg = Ridge(alpha=10)
+ridgeReg.fit(X_train,y_train)
+test_score_ridge = ridgeReg.score(X_test, y_test)
+print("The test score for ridge model is {}".format(test_score_ridge))
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Classification Models
- Work showcasing my classification skills:
+ Work showcasing my Machine Learning skills:
  
  Marital Status 
   I began predicting Marital status in a HR analytics dataset. I used random forest regression and before tuning I got 64% and after tuning I got 65% accuracy. After increasing size of training data my score went to 69%. I also tried KNN model where accuracy was worse.
 
 Department
   I then tried to predict the job role of an individual, again beginning with randomForest where I got accuracy of 0.55. I tried gradient boosting where I got a much better 0.65 before tuning.
+
+#Regression Models
+  
+ Insurance charges
+ Using the Insurance.csv I calculated the charges at a score of 0.736 using Ridge regression. I created a heatmap seeing that BMI, smoking and Age have the greatest correlation with charges. I scaled the data, converted categorical to numerical
